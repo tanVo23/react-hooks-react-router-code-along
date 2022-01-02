b425b708d6a462d5427c58df8cbb299418b52e2d
Tan <tan10tvo@gmail.com>
2022-01-02 11:40:19 -0700

    commit tree 29c16b1a7428e6c319e9b5406fdd94e3b2ee0c43

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App"
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+
+
+
+
+ReactDOM.render(
+  <BrowserRouter>
+  
+  <App />
+
+  </BrowserRouter>,
+  document.getElementById("root")
+);
